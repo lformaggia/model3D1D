a5ad9a9f7172c1cd2e8c5d93aa280c1061149bf1
PACS Student <interjo@hotmail.it>
2017-06-01 11:21:54 +0200

    Makefile.inc restructuration

--- GraphGenerator/src/SingleBranch/main.cpp
+++ GraphGenerator/src/SingleBranch/main.cpp
@@ -1,5 +1,5 @@
 #include "curved_network_reader.hpp"
-#include "c_problem3d1d.hpp"
+//#include "c_problem3d1d.hpp"
 
 
 
@@ -7,13 +7,13 @@ int main(int argc, char* argv[]){
  //Creating the input mesh file
  NetDiff::curved_reader(argc, argv);
  //Creating, solving and exporting the problem results
- 
+ /*
  getfem::c_problem3d1d prob;
  prob.init(argc, argv);
  prob.assembly();
  prob.solve();
  prob.export_vtk();
- 
+ */
  return 0;
 
 }
